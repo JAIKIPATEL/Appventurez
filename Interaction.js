d6c544251073f052933ef0618d9f4171cf5e991b
JAIKIPATEL <jaikipatel@gmail.com>
2022-06-01 15:52:09 +0530

    First Commit including interaction and operator

---
+++ Interaction.js
@@ -0,0 +1,11 @@
+//Alert 
+window.alert("please enter the details");
+// prompt
+let age = prompt('how old are you?');
+alert(`You are ${age} years old!`); // You are years old!
+
+let homeTown = prompt('please enter your homeTown'); // enter your homeTown
+alert(`your homeTown is ${homeTown}`);
+//confirm
+let details = confirm("Are you sure?");
+alert( details ); // true if OK is pressed
---
+++ stringadd.js
@@ -0,0 +1,4 @@
+let a = "1"; // prompt("First number?", 1);
+let b = "2"; // prompt("Second number?", 2);
+
+console.log(a + b); // 12
